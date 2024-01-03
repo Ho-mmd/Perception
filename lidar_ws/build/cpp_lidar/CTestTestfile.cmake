6b607181abab7698fa14cc68d41fb34d3d28f31b
lalywr2000 <12190450@inha.edu>
2024-01-03 19:07:13 +0100

    #2 Writing cpp lidar code

--- lidar_ws/build/cpp_lidar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ha/Desktop/ADS-Perception/lidar_ws/src/cpp_lidar
-# Build directory: /home/ha/Desktop/ADS-Perception/lidar_ws/build/cpp_lidar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- lidar_ws/build/cpp_lidar/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/ha/Desktop/ADS-Perception/lidar_ws/build/cpp_lidar/cpp_lidar" "TARGETS" "cpp_lidar" "DESTINATION" "lib/cpp_lidar")
