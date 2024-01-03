d3af32e95fa3bf2d6fd131af97d9cf4f05c63222
lalywr2000 <12190450@inha.edu>
2024-01-03 14:21:13 +0100

    #1 Change repo name

---
+++ lidar_ws/build/cpp_lidar/CMakeFiles/cpp_lidar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_lidar.dir/src/cpp_lidar.cpp.o"
+  "cpp_lidar"
+  "cpp_lidar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_lidar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lidar_ws/build/cpp_lidar/ament_cmake_symlink_install_targets_0_.cmake
@@ -0,0 +1,1 @@
+ament_cmake_symlink_install_targets("TARGET_FILES" "/home/ha/Desktop/ADS-Sense/lidar_ws/build/cpp_lidar/cpp_lidar" "TARGETS" "cpp_lidar" "DESTINATION" "lib/cpp_lidar")
