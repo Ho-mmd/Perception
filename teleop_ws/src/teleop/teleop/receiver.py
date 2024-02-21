a4076c3d23c4ed210b70a437deac2d093c89df68
lalywr2000 <12190450@inha.edu>
2024-02-21 17:50:36 +0100

    a

--- teleop_ws/src/teleop/teleop/receiver.py
+++ teleop_ws/src/teleop/teleop/receiver.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
-from example_interfaces.msg import Int8
+from geometry_msgs.msg import Twist
 
 from piracer.vehicles import PiRacerStandard
 
@@ -9,21 +9,15 @@ from piracer.vehicles import PiRacerStandard
 class TeleopSubscriber(Node):
     def __init__(self):
         super().__init__('receiver_node')
-        self.subscription_1 = self.create_subscription(Int8, 'steering_data', self.steering_callback, 10)
-        self.subscription_2 = self.create_subscription(Int8, 'throttle_data', self.throttle_callback, 10)
-        self.subscription_1  # prevent unused variable warning
-        self.subscription_2  # prevent unused variable warning
+        self.subscription_ = self.create_subscription(Twist, '/piracer/cmd_vel', self.callback, 10)
+        self.subscription_  # prevent unused variable warning
 
         self.piracer = PiRacerStandard()
 
 
-    def steering_callback(self, steering_msg):
-        self.piracer.set_steering_percent(steering_msg.data * -1.0)
-        # self.get_logger().info('')
-
-
-    def throttle_callback(self, throttle_msg):
-        self.piracer.set_throttle_percent(throttle_msg.data * 0.5)
+    def callback(self, control_msg):
+        self.piracer.set_steering_percent(control_msg.angular.z * -1.0)
+        self.piracer.set_throttle_percent(control_msg.linear.x * 0.5)
         # self.get_logger().info('')
 
 
