f6f1c47d9409ca006b5b3c048b3dfa0de0812a30
lalywr2000 <12190450@inha.edu>
2024-03-11 18:10:16 +0100

    #9 Failed to connect bcx24 result

--- perception_ws/src/detection/detection/detection.py
+++ perception_ws/src/detection/detection/detection.py
@@ -5,6 +5,7 @@ from sensor_msgs.msg import Image
 
 import  cv2
 from cv_bridge import CvBridge
+import torch
 
 
 class Detection(Node):
@@ -18,6 +19,30 @@ class Detection(Node):
 
     def callback(self, msg):
         image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+        model_path = "../model/model.pt"
+        model = torch.hub.load("./yolov5", "custom", path=model_path, source='local')
+
+        image = cv2.resize(image, (640, 640))
+        image = image.astype('float32') / 255.0
+        img_tensor = torch.from_numpy(image).permute(2, 0, 1).unsqueeze(0)
+            
+        with torch.no_grad():
+            output = model(img_tensor)
+
+            print(output)
+
+            # for i in range(output.size(1)):
+            #     detection = output[0, i]
+
+                # x_center = detection[0].item()
+                # y_center = detection[1].item()
+                # width = detection[2].item()
+                # height = detection[3].item()
+                # conf = detection[4].item()
+
+                # light = detection[5].item()
+                # sign = detection[6].item()
 
         cv2.imshow('Image', image)
         cv2.waitKey(1)
@@ -38,31 +63,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
-
-
-# import torch
-
-
-# def predict(self, frame):
-#     model_path = "./plugins/color_disability/model/bests.pt"
-#     model = torch.hub.load("./yolov5", "custom", path=model_path, source='local')
-
-#     frame = cv2.resize(frame, (640, 640))
-#     resized_image = frame.astype('float32') / 255.0
-#     img_tensor = torch.from_numpy(resized_image).permute(2, 0, 1).unsqueeze(0)
-    
-#     with torch.no_grad():
-#         output = model(img_tensor)
-
-#     for i in range(output.size(1)):
-#         detection = output[0, i]
-
-#         x_center = detection[0].item()
-#         y_center = detection[1].item()
-#         width = detection[2].item()
-#         height = detection[3].item()
-#         conf = detection[4].item()
-
-#         light = detection[5].item()
-#         sign = detection[6].item()
